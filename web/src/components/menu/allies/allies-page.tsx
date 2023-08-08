fbe3a2fef6494ade7ac8276aac3556a87971d03c
Nagodo <89977444+Nagodo@users.noreply.github.com>
2023-08-08 14:44:40 +0200

    ally stuff

--- web/src/components/menu/allies/allies-page.tsx
+++ web/src/components/menu/allies/allies-page.tsx
@@ -13,10 +13,19 @@ function LoadingAllies() {
 
 interface IAlly {
     id: number;
+    type: number;
+}
+
+function AddNewAlly() {
+    return (
+        <div className="newally">
+            <p className='info name'>Add new ally</p>
+        </div>
+    )
 }
 
 export function AlliesPage() {
-    const [allies, setAllies] = useState<IAlly[]>([{id: 1},{id: 1},{id: 1},{id: 1},{id: 1},{id: 1}])
+    const [allies, setAllies] = useState<IAlly[]>([{id: 1, type: 1},{id: 1, type: 1},{id: 1, type: 2},{id: 1, type: 3},{id: 1, type: 1}])
 
     if (allies === undefined || allies === null || allies.length === 0) {
         return (
@@ -28,24 +37,11 @@ export function AlliesPage() {
 
     return (
         <div className="allies">
-            <div className='section list'>
-                <div className='title'>
-                    <p>Allies</p>
-                </div>
+            <AddNewAlly />
+            {allies.map((ally) => {
+                return <Ally key={ally.id} type={ally.type} />
+            })}
                 
-                <div className='wrapper'>
-                    {allies.map((ally) => {
-                        return <Ally key={ally.id} />
-                    })}
-                </div>
-                
-            </div>
-
-            <div className='section pending'>
-
-            </div>
-
-            
         </div>
     );
 }
--- web/src/components/menu/allies/ally.tsx
+++ web/src/components/menu/allies/ally.tsx
@@ -1,5 +1,50 @@
-interface AllyProps {
+
+interface AllyBtnsProps {
+    type: number
+}
+
+interface ButtonProps {
+    text: string;
+    color: string;
+}
+
+function AllyBtns(props: AllyBtnsProps) {
+
+    function Button(props: ButtonProps) {
+        let classname = "button " + props.color
+        return (
+            <div className={classname}>
+                <p className="text">{props.text}</p>
+            </div>
+        )
+    }
     
+    if (props.type === 1) {
+        return (
+            <div className="buttons">
+                <Button text = "Deny" color = "red" />
+                <Button text = "Accept" color = "green" />
+            </div>
+        )
+    } else if (props.type === 2) {
+        return (
+            <div className="buttons">
+                <Button text = "Cancel" color = "red" />
+            </div>
+        )
+    } else if (props.type === 3) {
+        return (
+            <div className="buttons">
+                <Button text = "Remove" color = "red" />
+            </div>
+        )
+    }
+
+    return null;
+}
+
+interface AllyProps {
+    type: number
 }
 
 export function Ally(props: AllyProps) {
@@ -7,7 +52,8 @@ export function Ally(props: AllyProps) {
     return (
         <div className="ally">
             <p className='info name'>Din mor</p>
-            
+            <p className="info extra">Pending</p>
+            <AllyBtns type = {props.type} />
         </div>
     )
 }
