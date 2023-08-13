bf3823fc69b9443f35019197a3a669d1cd7c1265
Nagodo <magnusschoendorff@gmail.com>
2023-08-13 17:37:29 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -26,13 +26,13 @@ function OpenMenu()
 end
 
 RegisterNUICallback('fetchMembers', function(data, cb)
-    local members = exports['nagodo-gangs']:GetAllMembers("oki")
+    local members = exports['nagodo-gangs']:GetAllMembers()
 
     cb(members)
 end)
 
 RegisterNUICallback('fetchRoles', function(data, cb)
-    local roles = exports['nagodo-gangs']:GetAllRoles("oki")
+    local roles = exports['nagodo-gangs']:GetAllRoles()
    
     cb(roles)
 end)
